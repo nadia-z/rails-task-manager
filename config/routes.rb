d11b2ea014d5129e4b7a2aea4f3e62dc67e2d21a
Nadia Zeissig <33801529+nadia-z@users.noreply.github.com>
2025-06-10 11:44:12 +0200

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,18 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
 
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "tasks", to: "tasks#index", as: :index
   # Defines the root path route ("/")
+  get 'tasks/:id', to: 'tasks#show',    as: :task
   # root "posts#index"
+  # GET the HTML form (pre-filled with restaurant attributes) for editing = 1 request
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # PATCH the parameters to update an existing restaurant = 1 request
+  patch "tasks/:id",  to: 'tasks#update'
+
+  delete "tasks/:id", to: 'tasks#destroy'
 end
